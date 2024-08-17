375a8dedb1a60f86b96993bd56cc4f83f01fa8d6
Iddo Rozenblum <iddo.rozenblum2@gmail.com>
2024-08-17 13:38:21 +0300

    add some things to the conttroler

--- Controller/ai_anonimaitor.py
+++ Controller/ai_anonimaitor.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import math
+import utils as ut
 from scipy.ndimage import rotate
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -23,7 +24,6 @@ class Anonimaitor():
             self.bg = np.array(cv2.resize(cv2.imread(self.bg_path),img.shape[:2][::-1]))
             self.mask = np.array(Image.open(self.mask_path))
             self.mask_eyes = np.argwhere(np.all(self.mask == [255,0,0,255], axis=-1))
-            print(self.mask_eyes)
             self.mask_eyes_dif = math.sqrt((self.mask_eyes[0,0]-self.mask_eyes[1,0])**2+(self.mask_eyes[0,1]-self.mask_eyes[1,1])**2)
             self.first_eye_center_dif = math.sqrt((self.mask_eyes[0,0]-len(self.mask)//2)**2 + (self.mask_eyes[0,1]-len(self.mask[0])//2)**2)
             self.first = False
@@ -49,7 +49,6 @@ class Anonimaitor():
             shadow = np.zeros(img.shape,dtype=np.uint8)
             img = np.where(condition,shadow,self.bg)
         else:
-            print(eyes)
             ps = [(ey[0]+ey[2]//2,ey[1]+ey[3]//2) for ey in eyes]
             cv2.circle(img,ps[0],5,[0,255,0],5)
             cv2.circle(img,ps[1],5,[0,255,0],5)
@@ -69,16 +68,84 @@ class Anonimaitor():
             lx = min(len(img[0])-put_point[1],len(temp_mask[0]))
             ly = min(len(img)-put_point[0],len(temp_mask))
             cond[put_point[0]:put_point[0]+ly,put_point[1]:put_point[1]+lx] = temp_mask[:ly,:lx,3] == 255
-            print(cond)
             #img = np.where(cond,img,self.bg)    
             for i1,y in enumerate(img):
                 for i2, x in enumerate(y):
                     x[:] = cond[i1][i2]*255
                         
-            print(f"mask shape: {self.mask.shape}")
+            #print(f"mask shape: {self.mask.shape}")
             print(temp_mask.shape)
             
         return img
+
+class Anonimaitor2():
+    def __init__(self, bg_path, mask_path) -> None:
+        self.face_cascade = None
+        self.eye_cascade = cv2.CascadeClassifier(CV2_PATH + '\haarcascade_eye_tree_eyeglasses.xml')
+        self.bg_path = bg_path
+        self.mask_path = mask_path
+        self.eyes_from_face = True
+        self.first = True
+        
+        #for recorve
+        self.rec_eyes = None
+        self.rec_frames_without_eyes = 0 
+    
+    def get_unconnected_image(self,code=-1):
+        return self.bg
+    
+    def find_faces(self,img):
+        if (self.face_cascade is None):
+            self.face_cascade = cv2.CascadeClassifier(CV2_PATH + '\haarcascade_frontalface_default.xml')
+        return self.face_cascade.detectMultiScale(img,1.3,4)
+    
+    def find_eyes(self, img):
+        eyes = self.eye_cascade.detectMultiScale(img,1.3,1)
+        if (len(eyes) == 2): 
+            self.rec_eyes = eyes
+            self.rec_frames_without_eyes = 0
+            return eyes
+        if (self.rec_frames_without_eyes > 10 or self.rec_eyes is None): return []
+        self.rec_frames_without_eyes += 1
+        return self.rec_eyes
+    
+    def anonimaite(self,img):
+        if (self.first):
+            self.bg = np.array(cv2.resize(cv2.imread(self.bg_path),img.shape[:2][::-1]))
+            self.bg = cv2.cvtColor(self.bg, cv2.COLOR_BGR2BGRA)
+            self.mask = np.array(Image.open(self.mask_path))
+            self.mask_eyes = np.argwhere(np.all(self.mask == [255,0,0,255], axis=-1))
+            self.mask_eyes_dif = math.sqrt((self.mask_eyes[0,0]-self.mask_eyes[1,0])**2+(self.mask_eyes[0,1]-self.mask_eyes[1,1])**2)
+            self.first = False
+        
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
+        
+        if (self.eyes_from_face):
+            faces = self.find_faces(gray)
+            #for face in faces: cv2.rectangle(img,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),[0,255,0])
+            if (len(faces) == 0): return self.get_unconnected_image(1)
+            face = max(faces,key=lambda x: x[2]*x[3])
+            cv2.rectangle(img,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),[0,255,0])
+            eyes = self.find_eyes(gray[face[0]:face[0]+face[2],face[1]:face[1]+face[2]])
+            eyes = [[eye[0]+face[1],eye[1]+face[0],eye[2],eye[3]] for eye in eyes]
+        else: eyes = self.find_eyes(gray)
+        for eye in eyes: cv2.rectangle(img,(eye[0],eye[1]),(eye[0]+eye[2],eye[1]+eye[3]),[0,0,255])
+        
+        if (len(eyes) != 2): return self.get_unconnected_image(2)
+        
+        eye1 = min(eyes,key = lambda eye: eye[0])
+        eye2 = max(eyes,key = lambda eye: eye[0])
+        ext_ratio = math.dist(eye1,eye2) / self.mask_eyes_dif
+        
+        rot_degree = 180 - math.atan2(eye1[1]-eye2[1],eye1[0]-eye2[0])*180/math.pi 
+        rot_mask = cv2.resize(self.mask,tuple(round(self.mask.shape[1-i]*ext_ratio) for i in range(2)))
+        rot_mask = rotate(rot_mask,rot_degree)
+        
+        #img[:,:] = [0,0,0,255]
+        ut.put_sub_image(img,rot_mask,tuple(round(eye1[i] - (self.mask_eyes[1][1-i] * ext_ratio)) for i in range(2)))
+        return img
+
 
 def getBackgroundMask(image):
     mp_selfie_segmentation = mp.solutions.selfie_segmentation
@@ -106,7 +173,7 @@ def removeBackground(image,backgroundImage,backgroundColor=(0,0,0)):
 
 
 if (__name__ == "__main__"):
-    an = Anonimaitor(r"assets\Soviet-era-GettyImages-89856241-1200x720.jpg",r"assets\an1.png")
+    an = Anonimaitor2(r"assets\Soviet-era-GettyImages-89856241-1200x720.jpg",r"assets\an1.png")
 
     cap = cv2.VideoCapture(0)
 
@@ -114,6 +181,8 @@ if (__name__ == "__main__"):
         rat, frame = cap.read()
 
         frame = an.anonimaite(frame)
+        #frame = cv2.cvtColor(frame,cv2.COLOR_BGRA2BGR)
+        print(frame.shape)
         cv2.imshow("img",frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'): 
--- Controller/utils.py
+++ Controller/utils.py
@@ -1,4 +1,5 @@
 import aiohttp
+import numpy as np
 from pynput.keyboard import KeyCode,Key
 
 async def send_post_request_async(url, data):
@@ -26,3 +27,42 @@ def key_to_int(key: KeyCode) -> int:
     if (key == Key.caps_lock): return 20
     if (key == Key.ctrl): return 19
     return ord(key.char)
+
+
+def put_sub_image(img: np.array, sub_img: np.array, start_point:tuple):
+    print(start_point)
+    y_start = max(0, start_point[1])
+    y_end = min(len(img), start_point[1] + len(sub_img))
+    x_start = max(0, start_point[0])
+    x_end = min(len(img[0]), start_point[0] + len(sub_img[0]))
+    sub_start_x = x_start-start_point[0]
+    sub_start_y = y_start-start_point[1]
+    return insert_image(img,sub_img[sub_start_y:(sub_start_y + (y_end-y_start)),sub_start_x:(sub_start_x + (x_end-x_start)),:],x_start,y_start)
+    img[y_start:y_end, x_start:x_end,:] = sub_img[sub_start_y:(sub_start_y + (y_end-y_start)),sub_start_x:(sub_start_x + (x_end-x_start)),:]
+    return  
+    for y in range(y_start,y_end):
+        for x in range(x_start, x_end):
+            py, px = y - y_start + sub_start_y, x - x_start + sub_start_x
+            if (sub_img[py][px][-1] != 255): continue
+            img[y][x] = sub_img[py][px]
+            
+def insert_image(background, overlay, x, y):
+    h, w = overlay.shape[:2]
+
+    # Blending based on alpha channel
+    alpha_overlay = overlay[:, :, 3] / 255.0
+    alpha_background = background[y:y+h, x:x+w, 3] / 255.0
+
+    # Normalize alphas
+    combined_alpha = alpha_overlay + alpha_background * (1 - alpha_overlay)
+    combined_alpha[combined_alpha == 0] = 1  # To avoid division by zero in normalization
+
+    # Compute weighted sums
+    for c in range(3):  # For each color channel
+        background[y:y+h, x:x+w, c] = (overlay[:, :, c] * alpha_overlay + 
+                                       background[y:y+h, x:x+w, c] * alpha_background * (1 - alpha_overlay)) / combined_alpha
+
+    # Update the alpha channel of the background
+    background[y:y+h, x:x+w, 3] = combined_alpha * 255
+
+    return background
